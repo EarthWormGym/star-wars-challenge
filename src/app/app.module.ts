3cd0a832639f744f3002c7f2ac774f3fd652227f
EarthWormGym <adam56.beresford@gmail.com>
2023-11-23 18:43:11 +0000

    generated components and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { DisplayComponent } from './display/display.component';
+import { StarWarsService } from './star-wars.service';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AppComponent,
+    SearchComponent,
+    DisplayComponent
+  ],
   imports: [
     BrowserModule
+  ],
+  providers: [
+    StarWarsService
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/display/display.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-display',
+  templateUrl: './display.component.html',
+  styleUrl: './display.component.css'
+})
+export class DisplayComponent {
+
+}
